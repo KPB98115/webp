23d75d8e5e87e6e6f4745274afd98f6c6321ced5
KPB <79642613+KPB98115@users.noreply.github.com>
2022-04-20 16:53:11 +0800

    hw5

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -1,19 +1,43 @@
 function myfunction() {
-    var query_origin = $("#origin :selected").text();
-    var query_destination = $("#destination :selected").text();
-    var system_origin;
-    var system_destination;
+    var schedule_guery_url = `https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/S2STravelTime/TYMC?%24top=1&%24format=JSON`;
 
-    var data = $.getJSON("https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/ODFare/TYMC?%24top=10&%24format=JSON", (data) => {
-        alert("API invoked.");
-    }).done((msg) => {
-      alert("data receive sussessful.");
-      console.log(msg);
-      console.log(msg[0].DestinationStationID);
+    //Fetch json data from MOTC Transport API V2 for instant metro schedule
+    $.getJSON(schedule_guery_url, () => {
+        console.log("API invoked.");
+    }).done((data) => {
+        function secToMin(sec) {
+            let h = Math.floor(sec / 3600);
+            let m = Math.floor(sec % 3600 / 60);
+            let s = Math.floor(sec % 3600 % 60);
+
+            return h+"Hour(s)"+m+"Min(s)"+s+"Sec(s)";
+        }
+
+        console.log("data receive sussessful.");
+        console.log(data);
+
+        $(".travel_time").text("--"+secToMin(data[0].TravelTimes[0].RunTime)+"->");
+
+        for (let i=0; i<20; i++) {
+            if (i != 19) {
+                var travelTime = secToMin(data[0].TravelTimes[i+1].RunTime);
+                var stationName = data[0].TravelTimes[i].ToStationName.Zh_tw;
+
+                var newStation = `<h3 class="station">${stationName}</h3>`;
+                var newTime = `<h3 class="travel_time">--${travelTime}-></h3>`;
+                $(".jit_schedule").append(newStation);
+                $(".jit_schedule").append(newTime);
+            }
+            else {
+                stationName = data[0].TravelTimes[19].ToStationName.Zh_tw;
+                var newStation = `<h3 class="station">${stationName}</h3>`;
+                $(".jit_schedule").append(newStation);
+            }
+        }
     }).fail((msg) => {
-        alert("data receive unsussessful.");
         console.log(msg);
     }).always(() => {
-        alert("finished.");
+        console.log("finished.");
     });
+    this.blur();
 }
