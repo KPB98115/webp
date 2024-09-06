b88af9ff3488412df64f191a531aa298908d91a2
Kingston <79642613+KPB98115@users.noreply.github.com>
2024-09-06 15:50:30 +0800

    Delete qz8/qz8 directory

--- qz8/qz8/src/helloCGU.js
+++
@@ -1,9 +0,0 @@
-function multiButton(num) {
-    var output = [];
-    for (let i=1; i<num; ++i) {
-        output.push(<button id={i}>Button number {i}</button>)
-    }
-    return output;
-}
-
-export default multiButton;
